f107defa43c71e23a3eedd1e806c68faa9125206
M1cha <custofreak@web.de>
2012-03-30 18:06:04 +0300

    - reboot system when you click "Go Back" in recovery so you won't get a bootmenu for virtual system which can be dangerous

--- system/bootmenu/2nd-system/fshook.bootrecovery.sh
+++ system/bootmenu/2nd-system/fshook.bootrecovery.sh
@@ -32,7 +32,8 @@ cp -r -f /system/bootmenu/recovery/res/* /res/
 cp -p -f /system/bootmenu/recovery/sbin/* /sbin/
 
 # [fshook]patch resources
-cp -r -f /system/bootmenu/2nd-system/res/* /res/
+cp -r -f /system/bootmenu/2nd-system/recovery/res/* /res/
+cp -p -f /system/bootmenu/2nd-system/recovery/sbin/* /sbin/
 
 if [ ! -f /sbin/recovery_stable ]; then
     ln -s /sbin/recovery /sbin/recovery_stable
