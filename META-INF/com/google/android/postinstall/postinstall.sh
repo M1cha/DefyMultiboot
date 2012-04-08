5dde48344b9a9a550b11731ff538c10503143b08
M1cha <custofreak@web.de>
2012-04-08 22:46:04 +0300

    changed postinstall-script for 2nd-system

--- META-INF/com/google/android/postinstall/postinstall.sh
+++ META-INF/com/google/android/postinstall/postinstall.sh
@@ -6,6 +6,16 @@ version_major=`echo "$version_string" | cut -d '.' -f1`
 version_minor=`echo "$version_string" | cut -d '.' -f2`
 version_patch=`echo "$version_string" | cut -d '.' -f3`
 
+# check for 2ndsystem-support
+supports_2ndsystem=false
+if [ $version_major -le 1 ];then
+  if [ $version_minor -le 1 ];then
+    if [ $version_patch -lt 8 ];then
+      supports_2ndsystem=true
+    fi
+  fi
+fi
+
 # check for _config.sh
 if [ ! -f /system/bootmenu/script/_config.sh ]; then
   cp -f /tmp/postinstall/_config.sh /system/bootmenu/script/_config.sh
@@ -13,16 +23,19 @@ if [ ! -f /system/bootmenu/script/_config.sh ]; then
 fi
 
 # install 2nd-boot softlink
-if [ $version_major -le 1 ];then
-  if [ $version_minor -le 1 ];then
-    if [ $version_patch -lt 8 ];then
-      rm -f /system/bootmenu/script/2nd-boot.sh
-      ln -s 2nd-system.sh /system/bootmenu/script/2nd-boot.sh
-    fi
-  fi
+if [ $supports_2ndsystem ];then
+  rm -f /system/bootmenu/script/2nd-boot.sh
+  ln -s 2nd-system.sh /system/bootmenu/script/2nd-boot.sh
 fi
 
 # check for multiboot-config
 if [ ! -f /system/bootmenu/config/multiboot.conf ];then
   cp -f /tmp/postinstall/multiboot.conf /system/bootmenu/config/multiboot.conf
+fi
+
+# set default-bootmode
+if [ $supports_2ndsystem ];then
+ echo "2nd-system" > /system/bootmenu/config/default_bootmode.conf
+else
+ echo "2nd-boot" > /system/bootmenu/config/default_bootmode.conf
 fi
