7b4751939fa9ed2ff60a21b36d0a61c397c28be2
m1cha <custofreak@web.de>
2012-06-12 14:50:04 +0200

    - fixed memory-leaks in kenrel-module - "stat()"-hook is working now but still disabled because it has crazy side-effects when we change the paths

--- multiboot/multiboot_main.c
+++ multiboot/multiboot_main.c
@@ -47,57 +47,80 @@ module_param(debug, int, 0);
 
 static bool hooked = false;
 
-char* multiboot_parse_filename(const char* name) {
-    char* ret = name;
+int multiboot_parse_filename(char ** name) {
+
+    char* newname = *name;
+    int ret = -1;
 
     // redirect NAND-partitions
-    if(strcmp(name, "/dev/block/mmcblk1p1")==0) strcpy(ret, "/fshook/nodes/mmcblk1p1");
-    else if(strcmp(name, "/dev/block/mmcblk1p2")==0) strcpy(ret, "/fshook/nodes/mmcblk1p2");
-    else if(strcmp(name, "/dev/block/mmcblk1p3")==0) strcpy(ret, "/fshook/nodes/mmcblk1p3");
-    else if(strcmp(name, "/dev/block/mmcblk1p4")==0) strcpy(ret, "/fshook/nodes/mmcblk1p4");
-    else if(strcmp(name, "/dev/block/mmcblk1p5")==0) strcpy(ret, "/fshook/nodes/mmcblk1p5");
-    else if(strcmp(name, "/dev/block/mmcblk1p6")==0) strcpy(ret, "/fshook/nodes/mmcblk1p6");
-    else if(strcmp(name, "/dev/block/mmcblk1p7")==0) strcpy(ret, "/fshook/nodes/mmcblk1p7");
-    else if(strcmp(name, "/dev/block/mmcblk1p8")==0) strcpy(ret, "/fshook/nodes/mmcblk1p8");
-    else if(strcmp(name, "/dev/block/mmcblk1p9")==0) strcpy(ret, "/fshook/nodes/mmcblk1p9");
-    else if(strcmp(name, "/dev/block/mmcblk1p10")==0) strcpy(ret, "/fshook/nodes/mmcblk1p10");
-    else if(strcmp(name, "/dev/block/mmcblk1p11")==0) strcpy(ret, "/fshook/nodes/mmcblk1p11");
-    else if(strcmp(name, "/dev/block/mmcblk1p12")==0) strcpy(ret, "/fshook/nodes/mmcblk1p12");
-    else if(strcmp(name, "/dev/block/mmcblk1p13")==0) strcpy(ret, "/fshook/nodes/mmcblk1p13");
-    else if(strcmp(name, "/dev/block/mmcblk1p14")==0) strcpy(ret, "/fshook/nodes/mmcblk1p14");
-    else if(strcmp(name, "/dev/block/mmcblk1p15")==0) strcpy(ret, "/fshook/nodes/mmcblk1p15");
-    else if(strcmp(name, "/dev/block/mmcblk1p16")==0) strcpy(ret, "/fshook/nodes/mmcblk1p16");
-    else if(strcmp(name, "/dev/block/mmcblk1p17")==0) strcpy(ret, "/fshook/nodes/mmcblk1p17");
-    else if(strcmp(name, "/dev/block/mmcblk1p18")==0) strcpy(ret, "/fshook/nodes/mmcblk1p18");
-    else if(strcmp(name, "/dev/block/mmcblk1p19")==0) strcpy(ret, "/fshook/nodes/mmcblk1p19");
-    else if(strcmp(name, "/dev/block/mmcblk1p20")==0) strcpy(ret, "/fshook/nodes/mmcblk120");
-    else if(strcmp(name, "/dev/block/mmcblk1p21")==0) strcpy(ret, "/fshook/nodes/mmcblk1p21");
-    else if(strcmp(name, "/dev/block/mmcblk1p22")==0) strcpy(ret, "/fshook/nodes/mmcblk1p22");
-    else if(strcmp(name, "/dev/block/mmcblk1p23")==0) strcpy(ret, "/fshook/nodes/mmcblk1p23");
-    else if(strcmp(name, "/dev/block/mmcblk1p24")==0) strcpy(ret, "/fshook/nodes/mmcblk1p24");
-    else if(strcmp(name, "/dev/block/mmcblk1p25")==0) strcpy(ret, "/fshook/nodes/mmcblk1p25");
+    if(strcmp(*name, "/dev/block/mmcblk1p1")==0) newname = "/fshook/nodes/mmcblk1p1";
+    else if(strcmp(*name, "/dev/block/mmcblk1p2")==0) newname = "/fshook/nodes/mmcblk1p2";
+    else if(strcmp(*name, "/dev/block/mmcblk1p3")==0) newname = "/fshook/nodes/mmcblk1p3";
+    else if(strcmp(*name, "/dev/block/mmcblk1p4")==0) newname = "/fshook/nodes/mmcblk1p4";
+    else if(strcmp(*name, "/dev/block/mmcblk1p5")==0) newname = "/fshook/nodes/mmcblk1p5";
+    else if(strcmp(*name, "/dev/block/mmcblk1p6")==0) newname = "/fshook/nodes/mmcblk1p6";
+    else if(strcmp(*name, "/dev/block/mmcblk1p7")==0) newname = "/fshook/nodes/mmcblk1p7";
+    else if(strcmp(*name, "/dev/block/mmcblk1p8")==0) newname = "/fshook/nodes/mmcblk1p8";
+    else if(strcmp(*name, "/dev/block/mmcblk1p9")==0) newname = "/fshook/nodes/mmcblk1p9";
+    else if(strcmp(*name, "/dev/block/mmcblk1p10")==0) newname = "/fshook/nodes/mmcblk1p10";
+    else if(strcmp(*name, "/dev/block/mmcblk1p11")==0) newname = "/fshook/nodes/mmcblk1p11";
+    else if(strcmp(*name, "/dev/block/mmcblk1p12")==0) newname = "/fshook/nodes/mmcblk1p12";
+    else if(strcmp(*name, "/dev/block/mmcblk1p13")==0) newname = "/fshook/nodes/mmcblk1p13";
+    else if(strcmp(*name, "/dev/block/mmcblk1p14")==0) newname = "/fshook/nodes/mmcblk1p14";
+    else if(strcmp(*name, "/dev/block/mmcblk1p15")==0) newname = "/fshook/nodes/mmcblk1p15";
+    else if(strcmp(*name, "/dev/block/mmcblk1p16")==0) newname = "/fshook/nodes/mmcblk1p16";
+    else if(strcmp(*name, "/dev/block/mmcblk1p17")==0) newname = "/fshook/nodes/mmcblk1p17";
+    else if(strcmp(*name, "/dev/block/mmcblk1p18")==0) newname = "/fshook/nodes/mmcblk1p18";
+    else if(strcmp(*name, "/dev/block/mmcblk1p19")==0) newname = "/fshook/nodes/mmcblk1p19";
+    else if(strcmp(*name, "/dev/block/mmcblk1p20")==0) newname = "/fshook/nodes/mmcblk120";
+    else if(strcmp(*name, "/dev/block/mmcblk1p21")==0) newname = "/fshook/nodes/mmcblk1p21";
+    else if(strcmp(*name, "/dev/block/mmcblk1p22")==0) newname = "/fshook/nodes/mmcblk1p22";
+    else if(strcmp(*name, "/dev/block/mmcblk1p23")==0) newname = "/fshook/nodes/mmcblk1p23";
+    else if(strcmp(*name, "/dev/block/mmcblk1p24")==0) newname = "/fshook/nodes/mmcblk1p24";
+    else if(strcmp(*name, "/dev/block/mmcblk1p25")==0) newname = "/fshook/nodes/mmcblk1p25";
 
     // redirect partition-symlinks. It would be better to hook the function which resolves symlinks when opening a file.
-    else if(strcmp(name, "/dev/block/system")==0) strcpy(ret, "/fshook/nodes/mmcblk1p21");
-	else if(strcmp(name, "/dev/block/userdata")==0) strcpy(ret, "/fshook/nodes/mmcblk1p25");
-	else if(strcmp(name, "/dev/block/cache")==0) strcpy(ret, "/fshook/nodes/mmcblk1p24");
-	else if(strcmp(name, "/dev/block/pkbackup")==0) strcpy(ret, "/fshook/nodes/mmcblk1p23");
-	else if(strcmp(name, "/dev/block/prek")==0) strcpy(ret, "/fshook/nodes/mmcblk1p22");
-	else if(strcmp(name, "/dev/block/kpanic")==0) strcpy(ret, "/fshook/nodes/mmcblk1p20");
-	else if(strcmp(name, "/dev/block/cid")==0) strcpy(ret, "/fshook/nodes/mmcblk1p19");
-	else if(strcmp(name, "/dev/block/misc")==0) strcpy(ret, "/fshook/nodes/mmcblk1p18");
-	else if(strcmp(name, "/dev/block/cdrom")==0) strcpy(ret, "/fshook/nodes/mmcblk1p17");
-	else if(strcmp(name, "/dev/block/recovery")==0) strcpy(ret, "/fshook/nodes/mmcblk1p16");
-	else if(strcmp(name, "/dev/block/boot")==0) strcpy(ret, "/fshook/nodes/mmcblk1p15");
-	else if(strcmp(name, "/dev/block/pds")==0) strcpy(ret, "/fshook/nodes/mmcblk1p7");
+    else if(strcmp(*name, "/dev/block/system")==0) newname = "/fshook/nodes/mmcblk1p21";
+    else if(strcmp(*name, "/dev/block/userdata")==0) newname = "/fshook/nodes/mmcblk1p25";
+    else if(strcmp(*name, "/dev/block/cache")==0) newname = "/fshook/nodes/mmcblk1p24";
+    else if(strcmp(*name, "/dev/block/pkbackup")==0) newname = "/fshook/nodes/mmcblk1p23";
+    else if(strcmp(*name, "/dev/block/prek")==0) newname = "/fshook/nodes/mmcblk1p22";
+    else if(strcmp(*name, "/dev/block/kpanic")==0) newname = "/fshook/nodes/mmcblk1p20";
+    else if(strcmp(*name, "/dev/block/cid")==0) newname = "/fshook/nodes/mmcblk1p19";
+    else if(strcmp(*name, "/dev/block/misc")==0) newname = "/fshook/nodes/mmcblk1p18";
+    else if(strcmp(*name, "/dev/block/cdrom")==0) newname = "/fshook/nodes/mmcblk1p17";
+    else if(strcmp(*name, "/dev/block/recovery")==0) newname = "/fshook/nodes/mmcblk1p16";
+    else if(strcmp(*name, "/dev/block/boot")==0) newname = "/fshook/nodes/mmcblk1p15";
+    else if(strcmp(*name, "/dev/block/pds")==0) newname = "/fshook/nodes/mmcblk1p7";
 
-    /*/ redirect batterystats
-	else if(strcmp(name, "/data/system/batterystats.bin")==0) strcpy(ret, "/fshook/mounts/data/system/batterystats.bin");
-	else if(strcmp(name, "/data/system/batterystats.bin.tmp")==0) strcpy(ret, "/fshook/mounts/data/system/batterystats.bin.tmp");*/
+    // does not work - why?
+    //else if(strcmp(*name, "/data/system/batterystats.bin")==0) newname = "/fshook/mounts/data/system/batterystats.bin";
+    //else if(strcmp(*name, "/data/system/batterystats.bin.tmp")==0) newname = "/fshook/mounts/data/system/batterystats.bin.tmp";
 
+    // redirect access to ".android_secure"-folder
+    /*else {
+		char *needle = "/sdcard/.android_secure";
+		char *replacement = "/fshook/mounts/secure/.android_secure";
+		char *pch = strstr(*name, needle);
+		if(pch!=NULL && (pch-*name)==0) {
+			// build new name
+			char buffer[strlen(replacement)+strlen(*name)-strlen(needle)];
+			sprintf(buffer, "%s%.*s", replacement, strlen(*name)-strlen(needle), *name + strlen(needle));
 
-    if(strcmp(name, ret)!=0)  {
-        printk("[multiboot] Redirected '%s'->'%s'\n", name, ret);
+			// copy name to pointer
+			newname = kmalloc(sizeof(buffer)+1, GFP_ATOMIC);
+			strcpy(newname, buffer);
+			ret=0;
+		}
+    }*/
+
+    if(strcmp(*name, newname)!=0)  {
+    	printk("[multiboot] Redirected '%s'->'%s'\n", *name, newname);
+    	kfree(*name);
+    	*name = newname;
+        if(ret<0) ret=1; // we want that our malloc'd pointer will be freed from outside
+    } else {
+    	ret = 0;
     }
 
     return ret;
@@ -107,16 +130,29 @@ char* multiboot_parse_filename(const char* name) {
 struct file *do_filp_open(int dfd, const char *pathname,
 		int open_flag, int mode, int acc_mode)
 {
-	pathname = multiboot_parse_filename(pathname);
-	return HOOK_INVOKE(do_filp_open, dfd, pathname, open_flag, mode, acc_mode);
+	//printk("[multiboot] fopen: '%s'\n", pathname);
+	struct file* ret;
+	char *tmp = kmalloc(strlen(pathname)+1, GFP_ATOMIC);strcpy(tmp, pathname);
+
+	int result = multiboot_parse_filename(&tmp);
+	ret = HOOK_INVOKE(do_filp_open, dfd, tmp, open_flag, mode, acc_mode);
+
+	if(!result)kfree(tmp);
+	return ret;
 }
 
 long do_mount(char *dev_name, char *dir_name, char *type_page,
 		  unsigned long flags, void *data_page)
 {
 	printk("[multiboot] mount %s on %s type %s - flags: %ld\n", dev_name, dir_name, type_page, flags);
-	dev_name = multiboot_parse_filename(dev_name);
-    return HOOK_INVOKE(do_mount, dev_name, dir_name, type_page, flags, data_page);
+	long ret;
+	char *tmp = kmalloc(strlen(dev_name)+1, GFP_ATOMIC);strcpy(tmp, dev_name);
+
+	int result = multiboot_parse_filename(&tmp);
+    ret = HOOK_INVOKE(do_mount, tmp, dir_name, type_page, flags, data_page);
+
+    if(!result)kfree(tmp);
+	return ret;
 }
 
 int do_vfs_ioctl(struct file *filp, unsigned int fd, unsigned int cmd,
@@ -138,31 +174,18 @@ int multiboot_user_path_at(int dfd, const char __user *name, unsigned flags,
 		 struct path *path)
 {
 	struct nameidata nd;
-	char *oldname = getname(name);
-	char *tmp = oldname;
-	char buffer[30];
-	int i;
+	char *tmp = getname(name);
 
-	// CM7 crashes on first boot if we're using the global method.
-	// redirect our nodes
-	for(i=1; i<=25; i++) {
-		// check if current device is a NAND-partition
-		sprintf(buffer, "/dev/block/mmcblk1p%d", i);
-		if(strcmp(name, buffer)==0) {
-			// redirect to our loopdevice-nodes
-			sprintf(buffer, "/fshook/nodes/mmcblk1p%d", i);
-			strcpy(tmp, buffer);
-			break;
-		}
-	}
-
-	int err = PTR_ERR(oldname);
-	if (!IS_ERR(oldname)) {
+	int err = PTR_ERR(tmp);
+	if (!IS_ERR(tmp)) {
 
 		BUG_ON(flags & LOOKUP_PARENT);
 
+		//printk("[multiboot] STAT: '%s'\n", tmp);
+		int result = multiboot_parse_filename(&tmp);
+
 		err = path_lookup(tmp, flags, &nd);
-		putname(oldname);
+		if(!result)kfree(tmp);
 
 		if (!err)
 			*path = nd.path;
@@ -203,7 +226,7 @@ struct hook_info g_hi[] = {
 	HOOK_INIT(do_filp_open),
 	HOOK_INIT(do_mount),
 	HOOK_INIT(do_vfs_ioctl),
-	//HOOK_INIT(vfs_fstatat), disabled because it's unstable and we currently don't need this
+	//HOOK_INIT(vfs_fstatat), //disabled because it's unstable and we currently don't need this
 	HOOK_INIT_END
 };
 
