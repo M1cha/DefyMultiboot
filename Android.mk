12974fe9c212a70b404a1f842d622a23f0a367e4
m1cha <custofreak@web.de>
2012-06-17 22:44:21 +0200

    using Android-buildsystem

---
+++ Android.mk
@@ -0,0 +1,43 @@
+
+multiboot_dir := external/multiboot
+
+multiboot: \
+	multiboot_clean \
+	multiboot_copy_to_temp \
+	multiboot_copy_to_product \
+	multiboot_standalone_update
+
+
+multiboot_clean:
+	rm -Rf $(PRODUCT_OUT)/system/bootmenu/2nd-system
+	rm -Rf $(PRODUCT_OUT)/system/bootmenu/script/2nd-system.sh
+	rm -Rf $(OUT_DIR)/multiboot_tmp
+	rm -f $(PRODUCT_OUT)/multiboot-standalone.zip
+	rm -f $(PRODUCT_OUT)/multiboot-standalone-signed.zip
+	
+multiboot_copy_to_temp:
+	# copy files to temp-directory
+	mkdir $(OUT_DIR)/multiboot_tmp
+	cp -R $(multiboot_dir)/system $(OUT_DIR)/multiboot_tmp/
+	
+	# clean files from gitignore-files
+	find $(OUT_DIR)/multiboot_tmp/ -type f -name ".gitignore" -exec rm -f {} \;
+	
+multiboot_copy_to_product:
+	cp -R $(OUT_DIR)/multiboot_tmp/system $(PRODUCT_OUT)/
+	
+multiboot_standalone_update:
+	# copy kernel-modules
+	mkdir -p $(OUT_DIR)/multiboot_tmp/system/bootmenu/2nd-system/kernel-modules
+	cp device/motorola/jordan/modules/symsearch/symsearch.ko $(OUT_DIR)/multiboot_tmp/system/bootmenu/2nd-system/kernel-modules/
+	cp device/motorola/jordan/modules/multiboot/multiboot.ko $(OUT_DIR)/multiboot_tmp/system/bootmenu/2nd-system/kernel-modules/
+	
+	# copy updater-script
+	cp -R $(multiboot_dir)/META-INF $(OUT_DIR)/multiboot_tmp/
+	
+	# build zip
+	cd $(OUT_DIR)/multiboot_tmp && zip -r ../../$(PRODUCT_OUT)/multiboot-standalone.zip *
+	
+	# sign zip
+	java -jar $(HOST_OUT_JAVA_LIBRARIES)/signapk.jar -w build/target/product/security/testkey.x509.pem build/target/product/security/testkey.pk8 $(PRODUCT_OUT)/multiboot-standalone.zip $(PRODUCT_OUT)/multiboot-standalone-signed.zip
+	
