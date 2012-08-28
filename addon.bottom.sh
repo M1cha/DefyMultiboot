3190a7428afe16afc9196e26034b20f418faa115
M1cha <sigmaepsilon92@gmail.com>
2012-08-28 14:35:38 +0300

    fix restore

--- addon.bottom.sh
+++ addon.bottom.sh
@@ -8,25 +8,30 @@ case "$1" in
     done
   ;;
   restore)
+
+    # copy new files
     list_files | while read FILE REPLACEMENT; do
       R=""
       [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
       [ -f "$C/$S/$FILE" ] && restore_file $S/"$FILE" "$R"
     done
-    
-    rm -f bin/lsof
-    rm -f bin/logwrapper
-    rm -f xbin/logwrapper
-    rm -f bootmenu/images/indeterminate1.png
-    rm -f bootmenu/images/indeterminate2.png
-    rm -f bootmenu/images/indeterminate3.png
-    rm -f bootmenu/images/indeterminate4.png
-    rm -f bootmenu/images/indeterminate5.png
-    rm -f bootmenu/images/indeterminate6.png
-    
-    ln -s /system/bootmenu/binary/lsof /system/bin/lsof
-    ln -s /system/bin/bootmenu /system/bin/logwrapper
+
+    # copy bootmenu-alias
+    cp /system/bin/bootmenu /system/bootmenu/binary/bootmenu
+
+    # create symlinks
+    rm -f /system/xbin/logwrapper
+    rm -f /system/bin/logwrapper
+    rm -f /system/bin/lsof
+    rm -f /system/bootmenu/images/indeterminate1.png
+    rm -f /system/bootmenu/images/indeterminate2.png
+    rm -f /system/bootmenu/images/indeterminate3.png
+    rm -f /system/bootmenu/images/indeterminate4.png
+    rm -f /system/bootmenu/images/indeterminate5.png
+    rm -f /system/bootmenu/images/indeterminate6.png
     ln -s /system/bin/logwrapper.bin /system/xbin/logwrapper
+    ln -s /system/bin/bootmenu /system/bin/logwrapper
+    ln -s /system/bootmenu/binary/lsof /system/bin/lsof
     ln -s indeterminate.png /system/bootmenu/images/indeterminate1.png
     ln -s indeterminate.png /system/bootmenu/images/indeterminate2.png
     ln -s indeterminate.png /system/bootmenu/images/indeterminate3.png
