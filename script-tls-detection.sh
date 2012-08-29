86ad9e96e5375c10a5fa844f447f3c916fda96f4
M1cha <custofreak@web.de>
2012-08-29 02:22:17 +0200

    fix build and add tls-detection

---
+++ script-tls-detection.sh
@@ -0,0 +1,10 @@
+#!/sbin/sh
+
+# if multiboot is not installed...
+if [ "`cat /system/bootmenu/script/pre_bootmenu.sh | grep -c '\.enabletls'`" -eq 0 ];then
+
+  # if rom needs tls-support
+  if [ "`cat /system/bootmenu/script/pre_bootmenu.sh | grep -c 'tls-enable\.ko'`" -ge 1 ];then
+    touch /system/bootmenu/.enabletls
+  fi
+fi
